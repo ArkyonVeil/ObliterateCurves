836e06a77ee7324fd42b1fd53d337043c0b6c62f
Numnut64 <samnut64@gmail.com>
2025-11-06 00:28:01 +1030

    squarify.js
    Updated to work more reliably.

--- squarify.js
+++ squarify.js
@@ -1,3 +1,6 @@
 var style = document.createElement('style');
 document.head.appendChild(style);
+
 style.sheet.insertRule('* {border-radius: 0px !important;}');
+style.sheet.insertRule('*::before {border-radius: 0px !important;}');
+style.sheet.insertRule('*::after {border-radius: 0px !important;}');
